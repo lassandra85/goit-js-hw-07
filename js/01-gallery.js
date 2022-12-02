95d94d503a8ea16836f8ea9c6bb466109db27fa8
lassandra85 <lassandra85@gmail.com>
2022-12-02 15:42:06 +0100

    add2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,8 +1,8 @@
-import { galleryItems } from './gallery-items';
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryList = document.querySelector(".gallery");
-const markup = galleryItems.map(({ preview, original, description }) => { return '<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /></a></div > ' }).join("");
+const markup = galleryItems.map(({ preview, original, description }) => { return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /></a></div >`}).join("");
 
 galleryList.innerHTML = markup;
 
@@ -14,7 +14,7 @@ function onModalShow(event) {
         return
     };
 
-    const instance = basicLightbox.create(`<img src="event.target.dataset.source" width="800" height="600">`, {onShow: (instance) => {window.addEventListener("keydown", onModalCloses)}, onClose: (instance) => {window.removeEventListener("keydown", onModalCloses) } });
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`, {onShow: (instance) => {window.addEventListener("keydown", onModalCloses)}, onClose: (instance) => {window.removeEventListener("keydown", onModalCloses) } });
     
     function onModalCloses(event) {
         if (event.code === 'Escape') {
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,7 +2,7 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 const galleryList = document.querySelector(".gallery");
-const markup = galleryItems.map(({ preview, original, description }) => { return '<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}" /></a>' }).join("");
+const markup = galleryItems.map(({ preview, original, description }) => { return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>` }).join("");
 
 galleryList.innerHTML = markup;
 
