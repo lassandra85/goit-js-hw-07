2930b6c657db6f415097613786d53e9c2aea7b7a
lassandra85 <lassandra85@gmail.com>
2024-03-08 12:03:12 +0100

    01-galleryJS

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,26 +1,53 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryList = document.querySelector(".gallery");
-const markup = galleryItems.map(({ preview, original, description }) => { return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /></a></div >`}).join("");
+const gallery = document.querySelector(".gallery");
 
-galleryList.innerHTML = markup;
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href = ${original}>
+    <img
+      class = "gallery__image"
+      src = ${preview}
+      data-source = ${original}
+      alt = ${description}
+    />
+  </a>
+</li>`
+  )
+  .join("");
 
-galleryList.addEventListener("click", onModalShow);
+gallery.insertAdjacentHTML("beforeend", markup);
 
-function onModalShow(event) {
-    event.preventDefault();
-    if (!event.target.classList.contains('gallery__image')){
-        return
-    };
+gallery.addEventListener("click", itemShow);
 
-    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}" width="800" height="600">`, {onShow: (instance) => {window.addEventListener("keydown", onModalCloses)}, onClose: (instance) => {window.removeEventListener("keydown", onModalCloses) } });
-    
-    function onModalCloses(event) {
-        if (event.code === 'Escape') {
-            instance.close(); 
-        } 
+function itemShow(evt) {
+  evt.preventDefault();
+
+  const itemUrl = evt.target.dataset.source;
+  const itemAlt = evt.target.alt;
+
+  const modal = basicLightbox.create(
+    `<div class="modal">
+        <img class = "gallery__image"
+            src = ${itemUrl}
+            alt = ${itemAlt}
+        />
+    </div>
+    `,
+    {
+      onShow: () => document.addEventListener("keydown", onEscapeKeyPress),
+      onClose: () => document.removeEventListener("keydown", onEscapeKeyPress),
     }
-    instance.show();
-}
+  );
+  modal.show();
 
+  function onEscapeKeyPress(evt) {
+    if (evt.code === "Escape") {
+      modal.close();
+    }
+  }
+  /* console.log(instance); */
+  /* console.dir(itemAlt); */
+}
