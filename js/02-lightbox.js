bd60d096e10011f0aeefda701cf28098ba51fe8e
lassandra85 <lassandra85@gmail.com>
2024-03-08 12:04:16 +0100

    02-ligtboxJS

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,29 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-const galleryList = document.querySelector(".gallery");
-const markup = galleryItems.map(({ preview, original, description }) => { return `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}" alt="${description}"/></a>` }).join("");
+const gallery = document.querySelector(".gallery");
 
-galleryList.innerHTML = markup;
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `<li class="gallery__item">
+  <a class="gallery__link" href = ${original}>
+    <img
+      class = "gallery__image"
+      src = ${preview}
+      alt = ${description}
+    />
+  </a>
+</li>`
+  )
+  .join("");
 
-galleryList = new SimpleLightbox(".gallery a", {
-  captions: true,
-  captionDelay: 250,
-  captionSelector: "img",
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const galleryShow = new SimpleLightbox(".gallery a", {
   captionsData: "alt",
-  captionPosition: "bottom",
+  captionDelay: 250,
+  doubleTapZoom: 5,
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
